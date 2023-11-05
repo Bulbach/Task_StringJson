7f7d443f07bc887a88588407b3344edd2739e556
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-11-05 22:28:44 +0300

    feature/string_json: created class locomotive

---
+++ src/main/java/org/example/entity/Locomotive.java
@@ -0,0 +1,27 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants
+public class Locomotive {
+
+    private UUID uuid;
+    private String model;
+    private String locomotiveNumber;
+
+    private enum typeLocomotive {
+        PASSENGER,
+        FREIGHT,
+        SHUNTING
+    }
+}
